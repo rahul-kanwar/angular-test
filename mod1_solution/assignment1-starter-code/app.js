1dd781637502f6eaca498674af94ba9c80666429
Rahul Kanwar <kanwar@usc.edu>
2017-03-03 19:18:05 -0500

    Module 1 Solution

---
+++ mod1_solution/assignment1-starter-code/app.js
@@ -0,0 +1,40 @@
+(
+  function(){
+    'use strict';
+
+  angular
+      .module('LunchCheck', [])
+      .controller("LunchCheckController", LunchCheckController);
+
+    LunchCheckController.$inject = ['$scope'];
+
+      function LunchCheckController($scope){
+        // $scope.label = "Rahul";
+        console.log($scope.input);
+        $scope.checkItems = function(){
+
+          if($scope.input == "" || $scope.input == null){
+            $scope.message = "Please enter data first";
+            $scope.type = "danger";
+          }
+          else {
+                var noOfItems = $scope.input.split(',');
+                var cnt = 0;
+                for(var i=0;i<noOfItems.length;i++){
+                  if(noOfItems[i].trim()!="")
+                    cnt = cnt+1;
+                  }
+                if(cnt > 3)
+                {
+                  $scope.message = "Too Much!";
+                  $scope.type = "warning";
+                }
+                else {
+                  $scope.message = "Enjoy!";
+                  $scope.type = "success";
+                }
+              }
+        }
+
+      }
+  })();
