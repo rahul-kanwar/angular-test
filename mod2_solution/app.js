85da10ec9e391c1cad0e84becbe480a02ea7f55c
Rahul Kanwar <kanwar@usc.edu>
2017-07-30 23:26:33 -0400

    Final commit

---
+++ mod2_solution/app.js
@@ -0,0 +1,76 @@
+(
+  function(){
+    'use strict';
+
+    angular.module('ShoppingListCheckOff', [])
+            .controller('ToBuyController', ToBuyController)
+            .controller('AlreadyBoughtController', AlreadyBoughtController)
+            .service('ShoppingListCheckOffService', ShoppingListCheckOffService);
+
+ToBuyController.$inject = ['ShoppingListCheckOffService'];
+function ToBuyController(ShoppingListCheckOffService){
+
+  var list = this;
+  // list.itemName = "";
+  // list.itemQuantity = "";
+
+  list.toBuyList = ShoppingListCheckOffService.getBuyItems();
+
+  list.purchased = function(itemIndex){
+    ShoppingListCheckOffService.purchased(itemIndex);
+  }
+}
+
+AlreadyBoughtController.$inject = ['ShoppingListCheckOffService'];
+function AlreadyBoughtController(ShoppingListCheckOffService){
+
+  var list = this;
+  // list.itemName = "";
+  // list.itemQuantity = "";
+
+  list.toBoughtList = ShoppingListCheckOffService.getBoughtItems();
+
+}
+
+function ShoppingListCheckOffService(){
+    var service = this;
+
+    var boughtItems = [];
+
+    var buyItems = [
+          {
+          name : "cookies",
+          quantity : 10
+          },
+          {
+          name : "chips",
+          quantity : 2
+          },
+          {
+          name : "donuts",
+          quantity : 6
+          },
+          {
+          name : "bagels",
+          quantity : 4
+          },
+          {
+          name : "candies",
+          quantity : 20
+          }
+    ];
+
+    service.purchased = function(itemIndex){
+      boughtItems.push(buyItems[itemIndex]);
+      buyItems.splice(itemIndex, 1);
+    };
+
+    service.getBuyItems = function(){
+      return buyItems;
+    };
+
+    service.getBoughtItems = function(){
+      return boughtItems;
+    };
+}
+  })();
